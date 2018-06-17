8b489b0fbcadaab1bfacd45dd559d7474af933b6
kmmcgowa <kevin@kevin-mcgowan.com>
2018-06-17 15:35:23 -0700

    media query fixes at mobile sizes

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,9 @@
 
 echo -e "\033[0;32mDeploying update to Github...\033[0m"
 
-hugo
-
-cd public
-
 git add .
 
-msg="rebuilding and publishing updates"
+msg="rebuilding and publishing updates - $(date '+%Y-%m-%d %H:%M:%S')"
 if [ $# -eq 1 ]
 	then msg="$1"
 fi
@@ -17,6 +13,4 @@ git commit -m "$msg"
 
 echo $( git status )
 
-git push origin master
-
-cd ..
+git push
